1b58a2ed7b6cf63361752448e6438c9b33930df4
studiosteve <steve.r.mclaughlin@accenture.com>
2016-09-29 15:01:29 -0700

    install hapi mongoose and middleware. Set up a server.

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const hapi = require('hapi'),
+	good = require('good'),
+	joi = require('joi'),
+	mongoose = require('mongoose'),
+	// user = require('./models/index');
+
+server.connection({port:4000});
+server.register({
+	register:good,
+	options:{
+		reporters:{
+			console:[{
+				module:'good-squeeze',
+				name:'Squeeze',
+				args:[{
+					response:'*',
+					log:'*'
+				}]
+			},{
+				module:'good-console'
+			},'stdout']
+		}
+	}
+}, err=>{
+	if (!err) {
+		throw err;
+	}
+
+	server.start(err=>{
+		console.log('Server running at: ',server.info.uri);
+	});
+});
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/testing',(err,db)=>{
+	if (!err) {
+		console.log('Connected to mongo');
+	}
+});

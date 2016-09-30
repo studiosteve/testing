c038cfb9aaedf19eb4cb08901052b6eb0e5c70df
studiosteve <steve.r.mclaughlin@accenture.com>
2016-09-30 09:44:45 -0700

    created user model

---
+++ models/Users.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+// User Schema
+var	userSchema = new Schema({
+		name:String,
+		email:Schema.Types.Mixed,
+		password:Schema.Types.Mixed
+});
+
+// Email validation
+// var eValidator = email=>{
+// 	var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+// 	return emailRegex.test(email.Schema.Types.Mixed);
+// };
+// User.schema.path('email').validate(validator, 'The e-mail field cannot be empty.');
+
+// User Model
+var User = mongoose.model('User',userSchema);
+
+
+module.exports = User;
